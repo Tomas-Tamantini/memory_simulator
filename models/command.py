def315f188497c78f89a0188e227671ad151852f
Tomas Tamantini <tomasbft@gmail.com>
2021-08-24 20:14:32 -0300

    Convert Command to dataclass

--- models/command.py
+++ models/command.py
@@ -1,8 +1,9 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
 class Command:
-    def __init__(self, is_write: bool):
-        self.is_write = is_write
-
-
-def decode_command(command: str) -> Command:
-    is_write = command[2] == '1'
-    return Command(is_write)
+    address: int
+    is_write: bool
+    data: Optional[str] = None
