c065f20cccea054f9db1d990389469d8d49a109b
Tomas Tamantini <tomasbft@gmail.com>
2021-08-24 20:45:45 -0300

    Create Memory class

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from .load_commands import get_commands_from_input_file
+from .memory import Memory
---
+++ models/memory.py
@@ -0,0 +1,6 @@
+class Memory:
+    def __init__(self):
+        self.reads = 0
+        self.writes = 0
+        self.hits = 0
+        self.misses = 0
