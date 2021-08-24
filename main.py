10decbe831ce3853ad7821da07eed604dd8ddb19
Tomas Tamantini <tomasbft@gmail.com>
2021-08-24 20:39:53 -0300

    Load input file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello world!')
+from models import get_commands_from_input_file
+
+
+def main(input_file_path: str):
+    try:
+        commands = get_commands_from_input_file(input_file_path)
+    except ValueError as err:
+        print(f'Error: {err}')
+        return
+
+
+if __name__ == '__main__':
+    # TODO: Read file name from argv
+    main('input_model.txt')
