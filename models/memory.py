05db7d11677387481238a5ab577161ced4944deb
Tomas Tamantini <tomasbft@gmail.com>
2021-08-24 20:58:55 -0300

    Create execute method

--- models/memory.py
+++ models/memory.py
@@ -1,6 +1,22 @@
+from typing import List
+
+from models.command import Command
+
+
 class Memory:
     def __init__(self):
         self.reads = 0
         self.writes = 0
         self.hits = 0
         self.misses = 0
+
+    def execute(self, commands: List[Command]) -> str:
+        for cmd in commands:
+            self._execute_command(cmd)
+        return ""
+
+    def _execute_command(self, command: Command) -> None:
+        if command.is_write:
+            self.writes += 1
+        else:
+            self.reads += 1
