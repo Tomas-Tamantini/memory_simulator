aec81ab43ccf163c3dba5bdecaba4ecbd061bf24
Tomas Tamantini <tomasbft@gmail.com>
2021-08-24 21:13:52 -0300

    Convert command to desired string format

--- models/command.py
+++ models/command.py
@@ -7,3 +7,8 @@ class Command:
     address: int
     is_write: bool
     data: Optional[str] = None
+
+    @property
+    def as_raw_str(self):
+        out_str = f'{self.address} {"1" if self.is_write else "0"}'
+        return out_str if not self.is_write else out_str + " " + self.data
