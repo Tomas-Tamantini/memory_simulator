c88701de0645f28310f6589021dd9135a22788c1
Tomas Tamantini <tomasbft@gmail.com>
2021-08-24 21:00:04 -0300

    Get and save output log

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from models import get_commands_from_input_file
+from models import get_commands_from_input_file, Memory
+
+OUTPUT_PATH = 'results.txt'
 
 
 def main(input_file_path: str):
+    # Initialize empty memory
+    memory = Memory()
+
+    # Load commands
     try:
         commands = get_commands_from_input_file(input_file_path)
     except ValueError as err:
         print(f'Error: {err}')
         return
+
+    # Execute commands and save output log
+    output_log = memory.execute(commands)
+    with open(OUTPUT_PATH, 'w') as f:
+        f.write(output_log)
 
 
 if __name__ == '__main__':
