aa43e434e72d122b6ad5ae1ba14d623a9b7a9549
Luís F. Miki <69601772+luisfmiki@users.noreply.github.com>
2021-09-03 11:55:02 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,6 @@ if g++ MemSimulator.c -o MemSimulator.out; then
     ./MemSimulator.out input_model.txt
     echo "Resultados escritos em result_C.txt"
 else
-    echo "Não foi possível compilar, g++ está instalado em máquina?"
+    echo "Não foi possível compilar, g++ está instalado em sua máquina?"
 fi
 
