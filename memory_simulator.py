078831b5d753ffc81138699d39a8a89c0a6a4780
Tomas Tamantini <tomasbft@gmail.com>
2021-09-06 19:09:08 -0300

    Read file name from args

--- memory_simulator.py
+++ memory_simulator.py
@@ -1,3 +1,5 @@
+import sys
+
 from python_implementation import get_commands_from_input_file, Memory
 
 OUTPUT_PATH = 'result.txt'
@@ -19,7 +21,15 @@ def main(input_file_path: str):
     with open(OUTPUT_PATH, 'w') as f:
         f.write(output_log)
 
+    print(f'Output file saved - {OUTPUT_PATH}')
+
 
 if __name__ == '__main__':
-    # TODO: Read file name from argv
-    main('python_implementation/tests/txt_tests/in5.txt')
+    args = sys.argv
+    input_path = 'input_model.txt' if len(args) < 2 else args[1]
+    if len(args) > 2 and 'c' in args[2].lower():
+        print(f'Running MemorySimulator (C implementation) - Input file: {input_path}')
+        # TODO: Run C implementation here
+    else:
+        print(f'Running MemorySimulator (Python implementation) - Input file: {input_path}')
+        main(input_path)
