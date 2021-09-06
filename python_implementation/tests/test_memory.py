b668e6e2d1e5df8c245b4781020134955d6a2908
Tomas Tamantini <tomasbft@gmail.com>
2021-09-06 19:09:28 -0300

    Test hits and misses for a few commands

--- python_implementation/tests/test_memory.py
+++ python_implementation/tests/test_memory.py
@@ -1,4 +1,5 @@
 from python_implementation.models import Memory, Command
+from python_implementation.models.load_commands import decode_command
 
 
 def test_memory_initializes_empty():
@@ -21,5 +22,17 @@ def test_read_command_increments_read_and_write_numbers():
     assert mem.reads == 3 and mem.writes == 4 and mem.misses + mem.hits == mem.reads
 
 
-def run_txt_tests():
-    pass
+def test_execution():
+    commands = """
+        5 1 00000000000000000000000000000101
+        5 0
+        12 1 00000000000000000000000000010010
+        25 0
+    """
+    decoded_commands = [decode_command(cmd) for cmd in commands.split('\n') if len(cmd.strip()) > 0]
+    mem = Memory()
+    mem.execute(decoded_commands)
+    assert mem.reads == mem.writes == 2
+    assert mem.hits == mem.misses == 1
+
+
