d0cd0aa07f3762ea0086ef08623e96832166dd71
Tomas Tamantini <tomasbft@gmail.com>
2021-09-07 14:52:34 -0300

    Update shell command

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 
+python3 memory_simulator.py -c
+
+# Para rodar sem prompt da CLI, com o nome do arquivo ja no comando:
+# python3 memory_simulator.py -c input_model.txt
+
+# Para rodar implementacao Python, remover argumento -c:
+# python3 memory_simulator.py input_model.txt
+
+
